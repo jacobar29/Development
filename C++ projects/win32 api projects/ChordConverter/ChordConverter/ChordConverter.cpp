1a2156b4afeff8a7ef4d6d6bea44cf18f5f18abe
jacobar29 <jack.b.2992@gmail.com>
2018-08-15 10:00:49 +0100

    Change GUI look and feel, replace transpose switch statement with is_base function
    the use of is_base function in place of the previous switch does not change functionality but is more compact and readable

--- C++ projects/win32 api projects/ChordConverter/ChordConverter/ChordConverter.cpp
+++ C++ projects/win32 api projects/ChordConverter/ChordConverter/ChordConverter.cpp
@@ -6,70 +6,13 @@
 #include <sstream>
 #include <fstream>
 #include <iostream>
-
-#define FILE_MENU_NEW  1
-#define FILE_MENU_OPEN  2
-#define FILE_MENU_EXIT  3
-#define TRANSPOSE_UP_BUTTON 4
-#define TRANSPOSE_DOWN_BUTTON 5
-#define CLEAR_BUTTON 6
-
-// forward declerations
-LRESULT CALLBACK WindowProcedure(HWND, UINT, WPARAM, LPARAM);
-
-void getChordsFromFile();
-void AddMenus(HWND);
-void AddControls(HWND);
-void transpose(HWND);
-void getInputText();
-void printChordDiagrams(HWND);
-void getModifier(int);
-void clearButton(HWND);
-
-HMENU hMenu;
-HWND inputEdit;
-HWND numBox;
-HWND outputWindow;
-
-std::vector<HWND> drawnChords;
-
-int windowWidth = 500;
-int windowHeight = 500;
-
-
-//represents a standard octave
-std::string chords[12] = { "A", "A#", "B", "C", "C#", "D", "D#", "E", "F", "F#", "G", "G#" };
-
-std::vector <std::string> inputChords; // chords taken from input section
-
-									   // chord diagrams section stuff
-std::vector <std::string> chordDiagrams; // chords read from file 
-std::vector <std::string> chordsToPrint; // holds transposed chords to be printed in diagrams section
-
-std::string tempChord;
-std::string outputChords; //string printed to chord area
-std::string rows[5];
-
-
-int rowLength; // ensures even results spacing
-
-std::string chord;
-std::string temp;
-
-std::string outString;
-std::string baseNote;
-std::string noteSuffix;
-
-int noteIndex;
-int newNoteIndex;
-int modifier;
-
+#include "ChordConverter.h"
 
 int WINAPI WinMain(HINSTANCE hInst, HINSTANCE hprevInst, LPSTR args, int ncmdshow)
 {
 
 	WNDCLASSW wc = { 0 };
-	wc.hbrBackground = (HBRUSH)COLOR_WINDOW;
+	wc.hbrBackground = CreateSolidBrush(BG_COLOR);
 	wc.hCursor = LoadCursor(NULL, IDC_ARROW);
 	wc.hInstance = hInst;
 	wc.lpszClassName = L"ChordFinder";
@@ -93,80 +36,115 @@ int WINAPI WinMain(HINSTANCE hInst, HINSTANCE hprevInst, LPSTR args, int ncmdsho
 
 LRESULT CALLBACK WindowProcedure(HWND hWnd, UINT msg, WPARAM wp, LPARAM lp)
 {
+
 	switch (msg)
 	{
-		// menu commands
-	case WM_COMMAND:
-		switch (wp)
-		{
-		case FILE_MENU_EXIT:
-			DestroyWindow(hWnd);
-			break;
-		case TRANSPOSE_UP_BUTTON:
-			getModifier(1);
-			transpose(hWnd);
-			break;
-		case TRANSPOSE_DOWN_BUTTON:
-			getModifier(0);
-			transpose(hWnd);
-			break;
-		case CLEAR_BUTTON:
-			clearButton(hWnd);
-			break;
-		}
+		
 
-		break;
 		// initialise menus, controls and chord arrays
 	case WM_CREATE:
 		getChordsFromFile();
-		AddMenus(hWnd);
+		loadImages(hWnd);
 		AddControls(hWnd);
+		SendMessage(hWnd, WM_SETICON, ICON_BIG, (LPARAM)logoImage);
+
 		break;
 		//on exit
 	case WM_DESTROY:
 		PostQuitMessage(0);
 		break;
+	 //handle window events
+	case WM_COMMAND:
+		switch (wp)
+		{
+		case TRANSPOSE_UP_BUTTON:
+			transpose(hWnd, 1);
+			break;
+		case TRANSPOSE_DOWN_BUTTON:
+			transpose(hWnd, 0);
+			break;
+		case CLEAR_BUTTON:
+			clearGUI();
+			break;
+		}
+    // look and feel stuff, these are called just before each element is drawn
+	case WM_CTLCOLORSTATIC:
+	{
+		HDC hdcStatic = (HDC)wp;
+		
+		
+		SetTextColor(hdcStatic, RGB(0, 0, 0));
+		SetBkColor(hdcStatic, BG_COLOR);
+		HBRUSH hbrBkgnd = CreateSolidBrush(BG_COLOR);
 
+		return (INT_PTR)hbrBkgnd;
+	}
+	  	break;
+
+	case WM_CTLCOLOREDIT:
+	{
+		HBRUSH hbrBkgnd;
+		HDC hdcStatic = (HDC)wp;
+		SetTextColor(hdcStatic, RGB(0, 0, 0));
+
+		
+		if (numBox ==(HWND) lp) 
+		{
+			hbrBkgnd = CreateSolidBrush(BG_COLOR);
+			SetBkColor(hdcStatic, BG_COLOR);
+
+		}
+		else
+		{
+			hbrBkgnd = CreateSolidBrush(EDIT_BG_COLOR);
+			SetBkColor(hdcStatic, EDIT_BG_COLOR);
+		}
+			return (INT_PTR)hbrBkgnd;
+
+	}
+
+	break;
+	
 	default:
 		return DefWindowProcW(hWnd, msg, wp, lp);
 
 	}
 }
 
-void AddMenus(HWND hWnd)
-{
-	hMenu = CreateMenu();
-	HMENU hFileMenu = CreateMenu();
-
-		AppendMenu(hFileMenu, MF_SEPARATOR, NULL, NULL);
-	AppendMenu(hFileMenu, MF_STRING, FILE_MENU_EXIT, L"Exit");
-
-	SetMenu(hWnd, hMenu);
-}
-
 void AddControls(HWND hWnd)
 {
-	CreateWindowW(L"Static", L"Enter Chords Here: ", WS_VISIBLE | WS_CHILD | WS_BORDER | SS_CENTER, 0, 0, windowWidth, 50, hWnd, NULL, NULL, NULL);
-	inputEdit = CreateWindowW(L"Edit", L"...", WS_VISIBLE | WS_CHILD | WS_BORDER | ES_MULTILINE | ES_AUTOVSCROLL, 10, 60, windowWidth, 100, hWnd, NULL, NULL, NULL);
-	CreateWindowW(L"Button", L"-1", WS_VISIBLE | WS_CHILD, 10, 160, 100, 50, hWnd, (HMENU)TRANSPOSE_DOWN_BUTTON, NULL, NULL);
-	numBox = CreateWindowW(L"Edit", L"0", WS_VISIBLE | WS_CHILD | WS_BORDER | ES_NUMBER, (int)((windowWidth / 2) - 25), 160, 50, 50, hWnd, NULL, NULL, NULL);
-	CreateWindowW(L"Button", L"+1", WS_VISIBLE | WS_CHILD, windowWidth - 110, 160, 100, 50, hWnd, (HMENU)TRANSPOSE_UP_BUTTON, NULL, NULL);
-	outputWindow = CreateWindowW(L"Edit", L"...", WS_VISIBLE | WS_CHILD | WS_BORDER | ES_AUTOVSCROLL, 10, 210, windowWidth, 50, hWnd, NULL, NULL, NULL);
+	CreateWindowW(L"Static", L"Enter Chords Here: ", WS_VISIBLE | WS_CHILD | SS_CENTER | SS_CENTER, 0, 20, windowWidth, 30, hWnd, NULL, NULL, NULL);
+	inputWindow = CreateWindowW(L"Edit", L"", WS_VISIBLE | WS_CHILD | ES_MULTILINE | WS_BORDER| ES_AUTOVSCROLL, 20, 60, windowWidth -55, 100, hWnd, NULL, NULL, NULL);
+	outputWindow = CreateWindowW(L"Edit", L"...", WS_VISIBLE | WS_BORDER | WS_CHILD | ES_AUTOVSCROLL, 20, 230, windowWidth - 55, 50, hWnd, NULL, NULL, NULL);
 
-	CreateWindowW(L"Button", L"Clear", WS_VISIBLE | WS_CHILD, windowWidth - 110, windowHeight-90, 100, 50, hWnd, (HMENU)CLEAR_BUTTON, NULL, NULL);
+	numBox = CreateWindowW(L"Edit", L"0", WS_VISIBLE | WS_CHILD |ES_CENTER| ES_NUMBER, (int)((windowWidth / 2) - 25), 170, 50, 30, hWnd, NULL, NULL, NULL);
+	SendMessageW(numBox, WM_SETFONT, WPARAM(hFont), TRUE);
+	SendMessageW(hWnd, WM_CTLCOLOREDIT, NULL, (LPARAM)numBox);
+	//======== Buttons =====
+	HWND minusBtn = CreateWindowW(L"Button", L"-1", WS_VISIBLE | WS_CHILD | BS_BITMAP, 20, 170, 100, 50, hWnd, (HMENU)TRANSPOSE_DOWN_BUTTON, NULL, NULL);
+   	SendMessage(minusBtn, BM_SETIMAGE, IMAGE_BITMAP, (LPARAM)mBtnImage);
+
+	HWND plusBtn = CreateWindowW(L"Button", L"+1", WS_VISIBLE | WS_CHILD | BS_BITMAP, windowWidth - 135, 170, 100, 50, hWnd, (HMENU)TRANSPOSE_UP_BUTTON, NULL, NULL);
+	SendMessage(plusBtn, BM_SETIMAGE, IMAGE_BITMAP, (LPARAM)pBtnImage); 
+   	
+	HWND clearButton = CreateWindowW(L"Button", L"Clear", WS_VISIBLE | WS_CHILD | BS_BITMAP, 20, 20, 70, 30, hWnd, (HMENU)CLEAR_BUTTON, NULL, NULL);
+	SendMessage(clearButton, BM_SETIMAGE, IMAGE_BITMAP, (LPARAM)cBtnImage);
+	
 
 }
 
 // get the modifier number(the amount to transpose chords by) 
 //state = 1 increments value
 //state = 0 decrements value
-void getModifier(int state)
+int getModifier(int state)
 {
-	//get modifier from box
+	int modifier;
+	//get modifier from box, convert to std::string
 	wchar_t wtemp[100];
 	GetWindowText(numBox, wtemp, 256);
 	std::wstring ws(wtemp);
 	std::string str(ws.begin(), ws.end());
+	
 	// increment / decrement value, reset if exceeds bounds	
 	if (state == 1)
 		modifier = stoi(str) +1;
@@ -181,25 +159,27 @@ void getModifier(int state)
 	wsprintfW(buffer, L"%d", modifier);
 
 	SetWindowText(numBox, buffer);
+	return modifier;
 }
 
 
 
 void getInputText()
-{
-	//get text from the inputWindow, store it in vector inputChords, seperating at spaces
-   	inputChords.clear();
+{	//get text from the inputWindow, store it in vector inputChords, seperating at spaces
+   	
+	inputChords.clear();
    
 	//get and convert type to std::string
 	wchar_t inText[100];
-	GetWindowText(inputEdit, inText, 100);
+	GetWindowText(inputWindow, inText, 100);
 	std::wstring ws(inText);
 	std::string textInput(ws.begin(), ws.end());
-	std::stringstream instream(textInput); // write all from instream to inputchords vector
-	
-	while (instream >> chord)
+	std::stringstream instream(textInput); 
+	std::string str;
+	// write all from instream to inputchords vector
+	while (instream >> str)
 	{
-		inputChords.push_back(chord);
+		inputChords.push_back(str);
 		if (instream.peek() == ' ')		 // delimit at space
 			instream.ignore();
 	}
@@ -209,6 +189,7 @@ void getInputText()
 		inputChords[i][0] = toupper(inputChords[i][0]);
 
 }
+
 // get chords from file and store them in the array to be called when printed
 void getChordsFromFile()
 {
@@ -224,17 +205,27 @@ void getChordsFromFile()
 
 }
 
-// transpose the chords independent of any post chord suffix, modifier must already be set using getModifier() function
-void transpose(HWND hWnd)
+//checks if c is base note or suffix
+bool is_base(char c)
 {
+	char bases[8] = { 'A', 'B','C','D','E','F','G','#' };
+	return std::find(std::begin(bases), std::end(bases), c) != std::end(bases);
+}
+
+// transpose the chords independent of any post chord suffix, modifier must already be set using getModifier() function
+void transpose(HWND hWnd, int state)
+{
+	int modifier = getModifier(state);
+	std::string baseNote;
+	std::string noteSuffix;
 	getInputText();
-	outString.clear();
 	chordsToPrint.clear();
-	
+	int newNoteIndex = 0;
+
 	//loop through vector
 	for (int i = 0; i < inputChords.size(); i++)
 	{
-		temp = inputChords[i];
+		std::string temp = inputChords[i];
 		//clear existing string data
 		baseNote.clear();
 		noteSuffix.clear();
@@ -242,36 +233,18 @@ void transpose(HWND hWnd)
 		//loop through chars in each chord, split into base and suffix
 		for (int j = 0; j < temp.length(); j++)
 		{
-			switch (temp[j])
-			{
-			case 'A': baseNote += temp[j];
-				break;
-			case 'B': baseNote += temp[j];
-				break;
-			case 'C': baseNote += temp[j];
-				break;
-			case 'D': baseNote += temp[j];
-				break;
-			case 'E': baseNote += temp[j];
-				break;
-			case 'F': baseNote += temp[j];
-				break;
-			case 'G': baseNote += temp[j];
-				break;
-			case '#': baseNote += temp[j];
-				break;
-			default: noteSuffix += temp[j];
-				break;
-			}
+			if (is_base(temp[j]))
+				baseNote += temp[j];
+			else
+				noteSuffix += temp[j];
 		}
-
+	
 		//find position of base in array
 		for (int k = 0; k < 12; k++)
 		{
 			if (chords[k] == baseNote)
 			{
-				noteIndex = k;
-				newNoteIndex = noteIndex + modifier;
+				newNoteIndex = k + modifier;
 				// loop back to start or finish if end of array is exceeded
 				while (newNoteIndex > 11)
 					newNoteIndex -= 12;
@@ -282,7 +255,6 @@ void transpose(HWND hWnd)
 
 		//append to output string and chordsToPrint
 		chordsToPrint.push_back((chords[newNoteIndex] + noteSuffix));
-
 	}
 
 	// clear outputwindow and write  string
@@ -292,19 +264,17 @@ void transpose(HWND hWnd)
 
 void printChordDiagrams(HWND hWnd)
 {
-	int pWidth = 60;
-	int pHeight = 100;
-	int posx = 10;
-	int posy = 270;
-	//clear gui and reset arrays
-	outputChords.clear();
+	std::string outputString; //string printed to outputWindow
+	
+	//starting x and y coords for first row
+	int posx = 20;	 
+	int posy = 290;
+
 	for (int i = 0; i < drawnChords.size(); i++)
 		SendMessage(drawnChords[i], STM_SETIMAGE, NULL, NULL);
+	
 	drawnChords.clear();
-
-	for (int i = 0; i < 6; i++)
-		rows[i].clear();
-
+	
 	// loop all chords to print
 	for (int i = 0; i < chordsToPrint.size(); i++)
 	{
@@ -312,52 +282,61 @@ void printChordDiagrams(HWND hWnd)
 		int j = 0;
 		while (j < chordDiagrams.size())
 		{
-
 			// if chord found
 			if (chordDiagrams[j].find(chordsToPrint[i] + " ") != std::string::npos)
 			{
+				outputString += chordDiagrams[j];	  //add to string to be printed in outputWindow
 				
-				outputChords += chordDiagrams[j];
+				//get and convert url to LPCWSTR
 				std::string bmpLocation = chordDiagrams[j + 1];
 				std::wstring stemp = std::wstring(bmpLocation.begin(), bmpLocation.end());
 				LPCWSTR sw = stemp.c_str();
-				// extract chord diagram to string
-				HWND chord = CreateWindowW(L"Static", NULL, WS_VISIBLE | WS_CHILD | SS_BITMAP, posx, posy, pWidth, pHeight, hWnd, NULL, NULL, NULL);
-				HBITMAP	chordImage = (HBITMAP)LoadImageW(NULL, sw, IMAGE_BITMAP, pWidth, pHeight, LR_LOADFROMFILE);
-				drawnChords.push_back(chord);
+				
+				//create window and display chord image bitmap
+				HWND chord = CreateWindowW(L"Static", NULL, WS_VISIBLE | WS_CHILD | SS_BITMAP, posx, posy, bmpWidth, bmpHeight, hWnd, NULL, NULL, NULL);
+				HBITMAP	chordImage = (HBITMAP)LoadImageW(NULL, sw, IMAGE_BITMAP, bmpWidth, bmpHeight, LR_LOADFROMFILE);
 				SendMessage(chord, STM_SETIMAGE, IMAGE_BITMAP, (LPARAM)chordImage);
 
+				drawnChords.push_back(chord); //add chordwindow to drawnchords array for clearing purposes
+			   
+				posx += bmpWidth; //update coordinates for next chord
 
-
-
-				posx += pWidth;
 				//move to next row when row is full
-				if (posx + pWidth > windowWidth)
+				if (posx + bmpWidth > windowWidth)
 				{
 					posx = 10;
-					posy += pHeight;
+					posy += bmpHeight;
 				}			
-			} 
+			} // end if chord found loop 
 			j++;
 		} 	// end while loop ( j < chordDiagrams.size())
 
 	}	  // end all chords to print loop
 	
 	// convert to correct type for output to outputWindow
-	std::wstring stemp = std::wstring(outputChords.begin(), outputChords.end());
+	std::wstring stemp = std::wstring(outputString.begin(), outputString.end());
 	LPCWSTR sw = stemp.c_str();
 	SetWindowText(outputWindow, sw);
 	
-
-
 }
 
-void clearButton(HWND hWnd)
-{
+//reset all gui elements
+void clearGUI()
+{  	
 	SetWindowText(outputWindow, L"");
-	SetWindowText(inputEdit, L"");
+	SetWindowText(inputWindow, L"");
 	SetWindowText(numBox, L"0");
 	for (int i = 0; i < drawnChords.size(); i++)
 		SendMessage(drawnChords[i], STM_SETIMAGE, NULL, NULL);
+	
+}
+
+void loadImages(HWND hWnd)
+{
+	mBtnImage = (HBITMAP)LoadImageW(NULL, L"Minus.bmp", IMAGE_BITMAP, 100, 50, LR_LOADFROMFILE);
+	pBtnImage = (HBITMAP)LoadImageW(NULL, L"Plus.bmp", IMAGE_BITMAP, 100, 50, LR_LOADFROMFILE);
+  	cBtnImage = (HBITMAP)LoadImageW(NULL, L"Clear.bmp", IMAGE_BITMAP, 70, 30, LR_LOADFROMFILE);
+	logoImage = (HBITMAP)LoadImageW(NULL, L"ChordConverter.ico", IMAGE_ICON, 70, 30, LR_LOADFROMFILE);
+
 
 }
--- C++ projects/win32 api projects/ChordConverter/ChordConverter/ChordConverter.h
+++ C++ projects/win32 api projects/ChordConverter/ChordConverter/ChordConverter.h
@@ -1,3 +1,46 @@
 #pragma once
 
 #include "resource.h"
+
+#define TRANSPOSE_UP_BUTTON 1
+#define TRANSPOSE_DOWN_BUTTON 2
+#define CLEAR_BUTTON 3
+
+#define BG_COLOR RGB(8, 93, 158)
+#define BUTTON_COLOR RGB(119, 127, 151)
+#define EDIT_BG_COLOR RGB(67, 136, 189)
+
+#define hFont CreateFontW (40, 0, 0, 0, FW_DONTCARE, FALSE, FALSE, FALSE, ANSI_CHARSET, OUT_DEFAULT_PRECIS, CLIP_DEFAULT_PRECIS, DEFAULT_QUALITY, DEFAULT_PITCH | FF_SWISS, L"Arial")
+
+
+LRESULT CALLBACK WindowProcedure(HWND, UINT, WPARAM, LPARAM);
+void AddControls(HWND);
+void loadImages(HWND);
+void getChordsFromFile();
+void getInputText();
+int getModifier(int);
+void transpose(HWND, int);
+void printChordDiagrams(HWND);
+void clearGUI();
+
+//GUI elements
+HWND inputWindow;
+HWND numBox;
+HWND outputWindow;
+
+HBITMAP logoImage;
+HBITMAP pBtnImage;
+HBITMAP mBtnImage;
+HBITMAP cBtnImage;
+
+//window and image dimensions
+int windowWidth = 500;
+int windowHeight = 500;
+int bmpWidth = 60;
+int bmpHeight = 100;
+
+std::string chords[12] = { "A", "A#", "B", "C", "C#", "D", "D#", "E", "F", "F#", "G", "G#" };	//represents a standard octave
+std::vector<HWND> drawnChords;	//vector of chord diagram windows drawn used to clear the gui
+std::vector <std::string> inputChords; // chords taken from input section
+std::vector <std::string> chordDiagrams; // chords read from file 
+std::vector <std::string> chordsToPrint; // holds transposed chords prior to printing
